4a542e07adbc9140a858b1fed7214c6a6c24d9a4
Gabriel Carneiro <133365361+GabrielsilvaCarneiro@users.noreply.github.com>
2024-04-12 16:53:05 -0300

    lista Exercícios 02

---
+++ Exercicio C++ 02/Exercicio C++ 02.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+
+	/*\\\\\\\\\\\\\\\\\\\\
+		 Exercicio 01*/
+
+//using namespace std;
+//
+//int main() {
+//    int quantidade = 25; /*ALTERAR A QUANTIDADE*/
+//    float preco_unitario, preco_total;
+//
+//    cout << "Quantidade de sorvetes comprados: ";
+//
+//    if (quantidade <= 25) {
+//        preco_unitario = 1.50;
+//    }
+//    else {
+//        preco_unitario = 1.25;
+//    }
+//
+//    preco_total = quantidade * preco_unitario;
+//
+//    cout << "O cliente vai pagar R$ " << preco_total << endl;
+//
+//    return 0;
+//}
+
+	/*\\\\\\\\\\\\\\\\\\\\
+		 Exercicio 02*/
+
+//using namespace std;
+//
+//int main() {
+//    int numero = 15; /*ALTERAR O VALOR*/
+//    int resultado;
+//
+//    if (numero > 10) {
+//        resultado = numero + 10 * 2;
+//    }
+//    else {
+//        resultado = numero + 9 * 3 - 8;
+//    }
+//
+//    cout << "O resultado sera: " << resultado << endl;
+//
+//    return 0;
+//}
+
+    /*\\\\\\\\\\\\\\\\\\\\
+         Exercicio 03*/
+
+//using namespace std;
+//
+//int main() {
+//    int numero = 10; /*ALTERE O VALOR*/
+//
+//    if (numero > 0) {
+//        cout << "O numero " << numero << " e positivo" << endl;
+//    }
+//    else if (numero < 0) {
+//        cout << "O numero" << numero << " e negativo" << endl;
+//    }
+//    else {
+//        cout << "O numero e igual a zero." << endl;
+//    }
+//
+//    return 0;
+//}
+
+	/*\\\\\\\\\\\\\\\\\\\\
+		 Exercicio 04*/
+
+//using namespace std;
+//
+//int main() {
+//    int numero1 = 20;
+//    int numero2 = 15;
+//
+//    if (numero1 > numero2) {
+//        cout << "O primeiro numero " << numero1 << " e maior que o segundo numero (" << numero2 << ")." << endl;
+//    }
+//    else if (numero2 > numero1) {
+//        cout << "O segundo numero " << numero2 << " e maior que o primeiro numero (" << numero1 << ")." << endl;
+//    }
+//    else {
+//        cout << "Os numeros sao iguais." << endl;
+//    }
+//
+//    return 0;
+//}
+
+	/*\\\\\\\\\\\\\\\\\\\\
+		 Exercicio 05*/
+
+//using namespace std;
+//
+//int main() {
+//    float nota1 = 7.5;
+//    float nota2 = 8.0;
+//    int media;
+//
+//    // Calcula a média
+//    media = (nota1 + nota2) / 2;
+//
+//    if (media >= 7.0) {
+//        cout << " aluno Aprovado a nota final sera " << media << endl;
+//    }
+//    else {
+//        cout << "aluno Reprovado a nota final sera " << media  << endl;
+//    }
+//
+//    return 0;
+//}
+
+
