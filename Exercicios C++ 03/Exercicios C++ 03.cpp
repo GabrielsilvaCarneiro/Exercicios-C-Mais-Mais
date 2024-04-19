5614f562329f898ab7b5a7071cc1d56e2c8dea2f
Gabriel Carneiro <133365361+GabrielsilvaCarneiro@users.noreply.github.com>
2024-04-19 14:59:05 -0300

    Exercicios 03

---
+++ Exercicios C++ 03/Exercicios C++ 03.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+	////////////////////
+	//	Exercicio 01
+
+//using namespace std;
+//
+//int NumeroMaior(int num1, int num2) {
+//	if (num1 > num2) {
+//		return num1;
+//	}
+//	else {
+//		return num2;
+//	}
+//}
+//
+//int main() {
+//	int num1, num2;
+//
+//	cout << "Digite o seu primeiro numero ";
+//	cin >> num1;
+//
+//	cout << "Digite o seu segundo numero ";
+//	cin >> num2;
+//
+//	int maior = NumeroMaior(num1, num2);
+//
+//	cout << "O maior numero sera: " << maior << endl;
+//
+//		return 0;
+//}
+
+	////////////////////
+	//	Exercicio 02
+
+//using namespace std;
+//
+//int main() {
+//	int quantidade;
+//
+//	cout << "Informe a quantidade de produtos do estoque: ";
+//		cin >> quantidade;
+//
+//		if (quantidade >= 100){
+//			cout << "a Quantidade do estoque e de " << quantidade << " produtos e ela e suficiente." << endl;
+//		}
+//		else if (quantidade >= 50) {
+//			cout << "a Quantidade do estoque e de " << quantidade << " produtos e deve haver atencao" << endl;
+//		}
+//		else {
+//			cout << "Faca o reabastecimento do estoque pois a quantidade de produtos e apenas de " << quantidade << " produtos." << endl;
+//		}
+//
+//		return 0;
+//}
+
+	////////////////////
+	//	Exercicio 03
+
+//using namespace std;
+//
+//int main() {
+//	int numero;
+//
+//	cout << "Informe o numero: ";
+//	cin >> numero;
+//
+//	if (numero > 0) {
+//		cout << "o numero " << numero << " e positivo." << endl;
+//	}
+//	else if (numero < 0) {
+//		cout << "o numero " << numero << " e negatico." << endl;
+//	} 
+//	else {
+//		cout << "o numero e igual a " << numero << endl;
+//	}
+//	
+//	return 0;
+//}
