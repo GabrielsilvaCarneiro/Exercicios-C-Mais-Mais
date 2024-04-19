a2e26a42d9b16afba5b6f4b9777f98f9e42af366
Gabriel Carneiro <133365361+GabrielsilvaCarneiro@users.noreply.github.com>
2024-04-19 16:48:55 -0300

    Exercicio 04

---
+++ Exercicio c++ 04/Exercicio c++ 04.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+
+    ///////////////////////
+    //   Exercicio 01
+
+//using namespace std;
+//
+//int main() {
+//    int numero;
+//
+//    cout << "Digite um número de 1 a 7: ";
+//    cin >> numero;
+//
+//    switch (numero) {
+//    case 1:
+//        cout << "Domingo" << endl;
+//        break;
+//    case 2:
+//        cout << "Segunda-Feira" << endl;
+//        break;
+//    case 3:
+//        cout << "Terca-Feira" << endl;
+//        break;
+//    case 4:
+//        cout << "Quarta-Feira" << endl;
+//        break;
+//    case 5:
+//        cout << "Quinta-Feira" << endl;
+//        break;
+//    case 6:
+//        cout << "Sexta-Feira" << endl;
+//        break;
+//    case 7:
+//        cout << "Sabado" << endl;
+//        break;
+//    default:
+//        cout << "Dia invalido" << endl;
+//        break;
+//    }
+//
+//    return 0;
+//}
+    
+    ///////////////////////
+    //   Exercicio 02
+
+//using namespace std;
+//
+//int main() {
+//    float num1, num2;
+//    char operacao;
+//
+//    cout << "Digite o primeiro numero: ";
+//    cin >> num1;
+//
+//    cout << "Digite o segundo numero: ";
+//    cin >> num2;
+//
+//    cout << "Digite a operacao desejada (+, -, * ou /): ";
+//    cin >> operacao;
+//
+//    float resultado;
+//
+//    switch (operacao) {
+//    case '+':
+//        resultado = num1 + num2;
+//        break;
+//    case '-':
+//        resultado = num1 - num2;
+//        break;
+//    case '*':
+//        resultado = num1 * num2;
+//        break;
+//    case '/':
+//        if (num2 != 0) {
+//            resultado = num1 / num2;
+//        }
+//        break;
+//    default:
+//        cout << "Operação invalida!" << endl;
+//        return 1;
+//    }
+//
+//    cout << "Resultado: " << resultado << endl;
+//
+//    return 0;
+//}
+
+    ///////////////////////
+     //   Exercicio 03
+
+//using namespace std;
+//
+//int main() {
+//    char letra;
+//
+//    cout << "Digite uma letra: ";
+//    cin >> letra;
+//
+//    if (letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U') {
+//        cout << "essa letra e vogal" << endl;
+//    }
+//    else {
+//        switch (letra) {
+//        case 'a':
+//        case 'e':
+//        case 'i':
+//        case 'o':
+//        case 'u':
+//            cout << "essa letra e vogal" << endl;
+//            break;
+//        default:
+//            cout << "essa letra e consoante" << endl;
+//            break;
+//        }
+//    }
+//
+//    return 0;
+//}
+
